fc3e22f6147d7c2e844042ccafe264d41e65e634
Soo <jstensix@naver.com>
2021-03-15 19:40:56 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Resssact
+          Learn Ressssadact
         </a>
       </header>
     </div>
