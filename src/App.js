6d2b39110b17202cd33a3bf02648f7b52709e7a9
Soo <jstensix@naver.com>
2021-03-15 19:37:37 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-slink"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Resssact
         </a>
       </header>
     </div>
