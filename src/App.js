4a3ecdfaf16382f75cd3cf60618f7bee24575de0
dofordev <31636655+dofordev@users.noreply.github.com>
2022-10-01 11:46:40 +0900

    Develop (#2)
    * commit1
    
    * commit2
    
    * commit5
    
    * commit6
    
    * commit8
    
    * commit8
    
    * develop1
    
    * develop2
    
    * develop3
    
    * develop4
    
    * develop5
    
    * develop6
    
    * developA
    
    * developB
    
    Co-authored-by: 김종수/주문개발팀 <jongsoo.kim@yanolja.com>

--- src/App.js
+++ src/App.js
@@ -15,20 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Ressssadacttest222
-          teasdasdasstdevleop213123
-          asdasdadevelop111
-          develop333
-          asdasdamaster111
-          develop22
-          Learn Ressssadacttest
-          test111
-          test222master111
-          Learn Ressssadacttest22asdasdsad2
-          testdevleop213123asdasda
-          develop22
-          devleop33
-          develop44
+         a
+          b
         </a>
       </header>
     </div>
