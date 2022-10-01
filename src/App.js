3b33701f4e98650f3e41116c2a058e27ccf7a58e
김종수/주문개발팀 <jongsoo.kim@yanolja.com>
2022-10-01 11:02:51 +0900

    commit1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Ressssadact
+          Learn Ressssadacttest
         </a>
       </header>
     </div>
